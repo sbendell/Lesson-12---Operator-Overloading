dd73ceeaa628d3ac92c36b26f14e8b656afcac72
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-11 13:24:03 +0100

    Copied code

--- Lesson 12 - Operator Overloading/Lesson 12 - Operator Overloading.cpp
+++ Lesson 12 - Operator Overloading/Lesson 12 - Operator Overloading.cpp
@@ -2,10 +2,34 @@
 //
 
 #include "stdafx.h"
+#include "Matrix.h"
 
+using namespace std;
 
 int main()
 {
-    return 0;
+	Matrix<int> mtx1;
+	mtx1.set_element(0, 0, 1);
+	mtx1.set_element(1, 1, 4);
+	mtx1.set_element(2, 2, 6);
+	cout << "matrix 1:\n" << mtx1;
+
+	Matrix<int> mtx2;
+	mtx2.set_element(0, 0, 1);
+	mtx2.set_element(1, 1, 4);
+	mtx2.set_element(2, 2, 2);
+
+	cout << "matrix 2:\n" << mtx2;
+
+	Matrix<int> mtx3;
+	mtx3 = mtx2;
+	cout << "matrix 3 assigned from matrix 2:\n" << mtx3;
+
+	Matrix<int> product = (mtx1 * mtx3);
+	cout << "matrix product assigned from mtx1 * mtx3:\n" << product;
+
+	int x;
+	cin >> x;
+	return 0;
 }
 
